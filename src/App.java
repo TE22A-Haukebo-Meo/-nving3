c30365c95c2b2164ff1e335cc0bb55e73a8898fd
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2025-01-08 10:17:19 +0100

    forstaättning

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        hello32();
+        /*hello32();
+        int bongus = 65;
+        coolers(bongus);*/
+        double uno = 4.3;
+        double dos = 5.7;
+        double quattro = multi(uno, dos);
+        System.out.println(quattro);
     }
 
     public static void hello32(){
         for (int i = 0; i < 32; i++) {
             System.out.println("Hello, world");
         }
+    }
+
+    public static void coolers(int bongus){
+        System.out.println(bongus*bongus);
+    }
+
+    public static double multi(double uno, double dos){
+        double tres = uno*dos;
+        return tres;
     }
 }
