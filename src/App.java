03881d1c1a5f47ace395fcb5f40a2021f05b766a
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2025-01-08 10:10:41 +0100

    börjat

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        hello32();
+    }
+
+    public static void hello32(){
+        for (int i = 0; i < 32; i++) {
+            System.out.println("Hello, world");
+        }
+    }
+}
